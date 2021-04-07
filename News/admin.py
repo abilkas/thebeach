95f68d9e8ca1a0d2807e5fc4aa1f375cdf691018
Abilkas <abilkasimov.site@gmail.com>
2021-04-07 22:39:03 +0500

    punkt 5

--- News/admin.py
+++ News/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from News.models import News
 # Register your models here.
-admin.site.register(News)
+
+class NewsAdmin(admin.ModelAdmin):
+	list_display = ('title', 'created_at')
+	list_filter = ('title', 'created_at')
+	#prepopulated_fields = {'slug': ('title', )}
+
+admin.site.register(News, NewsAdmin)
